89117b0e9811ab27f4edaff96e7664e88425c906
VicenteZanatta <vicentezanatta03@gmail.com>
2025-12-04 11:08:06 -0300

    changed block ip

--- sqlInjectionMonitor.py
+++ sqlInjectionMonitor.py
@@ -3,8 +3,9 @@ import pyshark
 import sys
 
 def sql_injection_monitor(arg_interface, arg_port):
-    print(f"Monitorando tráfego na interface {arg_interface}, porta {arg_port}...")
-    print("Pressione Ctrl+C para parar\n")
+    print(f"Monitoring traffic in {arg_interface}, port {arg_port}...")
+    print("Ctrl+C to stop\n")
+    
     
     try:
         capture = pyshark.LiveCapture(interface = arg_interface, 
@@ -16,27 +17,32 @@ def sql_injection_monitor(arg_interface, arg_port):
                     block_packet_source_ip(packet.ip.src)       # if payload has SQL injection, block sorce ip traffic at iptrables      
 
     except KeyboardInterrupt:
-        print(f"\nMonitoramento finalizado")
+        print(f"\nMonitor Stopped")
+        print(f"Captured Packeges {len(capture)}")
+
     except Exception as e:
-        print(f"Erro: {e}")
-        print("Verifique se a interface e porta são válidas e se você tem permissões para capturer pacotes")
+        print(f"Error: {e}")
+        print("Verify that the interface and port are valid and that you have permissions for packet capture")
 
 
 def payload_has_injection(payload):
             
-            sql_pattern = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'FROM', 'WHERE', 
-                            'CREATE', 'DROP', 'ALTER', 'JOIN', 'UNION', 'EXEC']
-            
-            #decode from hex to humam readable text
-            hex_split = payload.split(':')
-            hex_as_chars = map(lambda hex: chr(int(hex, 16)), hex_split)
-            payload_human_readable = ''.join(hex_as_chars)
-            print(f'Decoded payload: {payload_human_readable}')
+    sql_pattern = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'FROM', 'WHERE', 
+                    'CREATE', 'DROP', 'ALTER', 'JOIN', 'UNION', 'EXEC']
+    
+    #decode from hex to humam readable text
+    hex_split = payload.split(':')
+    hex_as_chars = map(lambda hex: chr(int(hex, 16)), hex_split)
+    payload_human_readable = ''.join(hex_as_chars)
+    #print(f'Decoded payload: {payload_human_readable}')
 
-            for pattern in sql_pattern:
-                if pattern in payload_human_readable.upper():
-                     print("SQL INJECTION DETECT")
-                     return True
+    # check for each SQL pattern in packet payload
+    for pattern in sql_pattern:
+        if pattern in payload_human_readable.upper(): 
+             print("SQL INJECTION DETECT")
+             return True
+        
+    return False
                 
 
 def block_packet_source_ip(source_ip):
@@ -46,8 +52,9 @@ def block_packet_source_ip(source_ip):
             print(f"IP {source_ip} blocked successfully!")
             
             # Block outgoing traffic to source_ip
-            subprocess.run(['sudo', 'iptables', '-A', 'OUTPUT', '-d', source_ip, '-j', 'DROP'], check=True)
-            print(f"Outgoing traffic to IP {source_ip} also blocked!")
+            subprocess.run(['sudo', 'iptables', '-t', 'raw', '-I', 'PREROUTING', '-s', source_ip, '-j', 'DROP'], check=True)
+            #subprocess.run(['sudo', 'iptables', '-A', 'OUTPUT', '-d', source_ip, '-j', 'DROP'], check=True)
+            #print(f"Outgoing traffic to IP {source_ip} also blocked!")
             
         except subprocess.CalledProcessError as e:
             print(f"Error blocking IP {source_ip}: {e}")    
@@ -55,7 +62,7 @@ def block_packet_source_ip(source_ip):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Uso: python script.py <interface> <porta>")
+        print("usage: python script.py <interface> <port>")
         sys.exit(1)
     
     try:
@@ -63,5 +70,5 @@ if __name__ == "__main__":
         porta = int(sys.argv[2])
         sql_injection_monitor(interface, porta)
     except ValueError:
-        print("Erro: A porta deve ser um número inteiro")
+        print("Error: invalid port number")
         sys.exit(1)

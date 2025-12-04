f09cecaaebc80793e7a9d08a8d0a854b8511fb69
VicenteZanatta <vicentezanatta03@gmail.com>
2025-12-04 15:31:55 -0300

    add hex patter to fastest detection

--- sqlInjectionMonitor.py
+++ sqlInjectionMonitor.py
@@ -30,33 +30,39 @@ def payload_has_injection(payload):
     sql_pattern = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'FROM', 'WHERE', 
                     'CREATE', 'DROP', 'ALTER', 'JOIN', 'UNION', 'EXEC']
     
+    hex_sql_pattern = ['53:45:4C:45:43:54', '49:4E:53:45:52:54:0A', '55:50:44:41:54:45',
+                       '44:45:4C:45:54:45', '46:52:4F:4D', '57:48:45:52:45', '43:52:45:41:54:45:0A',
+                       '44:52:4F:50:0A', '41:4C:54:45:52', '4A:4F:49:4E' '55:4E:49:4F:4E', '45:58:45:43']
+    
     #decode from hex to humam readable text
-    hex_split = payload.split(':')
-    hex_as_chars = map(lambda hex: chr(int(hex, 16)), hex_split)
-    payload_human_readable = ''.join(hex_as_chars)
-    #print(f'Decoded payload: {payload_human_readable}')
+    # hex_split = payload.split(':')
+    # hex_as_chars = map(lambda hex: chr(int(hex, 16)), hex_split)
+    # payload_human_readable = ''.join(hex_as_chars)
+    # print(f"hex payload: \n{payload}")
+    # print(f'Decoded payload: {payload_human_readable}')
 
-    # check for each SQL pattern in packet payload
-    for pattern in sql_pattern:
-        if pattern in payload_human_readable.upper(): 
-             print("SQL INJECTION DETECT")
-             return True
-        
+
+    # # check for each SQL pattern in packet payload
+    # for pattern in sql_pattern:
+    #     if pattern in payload_human_readable.upper(): 
+    #          print("SQL INJECTION DETECT")
+    #          return True
+
+    for hex_pattern in hex_sql_pattern:
+        if hex_pattern in payload:
+            print("!! SQL INJECTION DETECT !!")
+            return True
+
     return False
                 
 
 def block_packet_source_ip(source_ip):
         try:
-            # Block incoming traffic from source_ip
-            subprocess.run(['sudo', 'iptables', '-A', 'INPUT', '-s', source_ip, '-j', 'DROP'], check=True)
-            print(f"IP {source_ip} blocked successfully!")
-            
+            print(f"IP {source_ip} blocked successfully!")            
             # Block outgoing traffic to source_ip
             subprocess.run(['sudo', 'iptables', '-t', 'raw', '-I', 'PREROUTING', '-s', source_ip, '-j', 'DROP'], check=True)
-            #subprocess.run(['sudo', 'iptables', '-A', 'OUTPUT', '-d', source_ip, '-j', 'DROP'], check=True)
-            #print(f"Outgoing traffic to IP {source_ip} also blocked!")
             
-        except subprocess.CalledProcessError as e:
+        except Exception as e:
             print(f"Error blocking IP {source_ip}: {e}")    
 
 
